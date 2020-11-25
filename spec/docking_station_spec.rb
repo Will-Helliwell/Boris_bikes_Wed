e3f690795ec4faf51fc580e11f749ad4f2125349
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2020-11-25 16:31:31 +0000

    add methods to dock and read docked bikes to a docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-  
+
   it "responds to release_bike" do
     expect(subject).to respond_to(:release_bike)
   end
@@ -12,6 +12,21 @@ describe DockingStation do
 
   it "bike is working" do
     expect(DockingStation.new.release_bike.working?).to eq(true)
+  end
+
+  it "responds to dock and shows the docked bike" do
+    bike = Bike.new
+    expect(subject).to respond_to(:dock).with(1).argument
+    expect(subject.dock(bike)).to eq(bike)
+  end
+
+  it "can show docked bikes when read with .bike" do
+    bike = Bike.new
+    station = DockingStation.new
+    station.dock(bike)
+
+    expect(subject).to respond_to(:bike)
+    expect(station.bike).to eq(bike)
   end
 
 end
