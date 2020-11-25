288bf60eef1421e30b79497cad798bc33a1e2738
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2020-11-25 14:31:58 +0000

    separate bike class into separate file

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,10 @@
 require 'docking_station.rb'
+require 'bike.rb'
 
 describe DockingStation do
+
   it "responds to release_bike" do
-  expect(DockingStation.new).to respond_to(:release_bike)
+    expect(DockingStation.new).to respond_to(:release_bike)
   end
+
 end
