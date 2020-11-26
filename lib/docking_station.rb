d756264f324d565db4cbfb8eacc5579e88b01ad4
William Helliwell <whelliwell1@MA306FV3953LYWG.home>
2020-11-26 12:43:47 +0000

    docking stations are created empty and will return an error when passed .release_bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,7 +3,8 @@ require './lib/bike'
 class DockingStation
 
   def release_bike
-    Bike.new
+    fail 'No bikes available' if @bike == nil
+    @bike
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,17 +2,26 @@ require 'docking_station.rb'
 
 describe DockingStation do
 
-  it "responds to release_bike" do
-    expect(subject).to respond_to(:release_bike)
+  describe '#release_bike' do
+    it "responds to release_bike" do
+      expect(subject).to respond_to(:release_bike)
+    end
+    it "gets a bike when relase_bike is called" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq(bike)
+    end
+    it "released bike is working" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike.working?).to eq(true)
+    end
+    it "rasies an error when called on an empty docking station" do
+      expect {subject.release_bike}.to raise_error('No bikes available')
+    end
   end
 
-  it "gets a bike when relase_bike is called" do
-    expect(DockingStation.new.release_bike).to be_a_kind_of(Bike)
-  end
 
-  it "bike is working" do
-    expect(DockingStation.new.release_bike.working?).to eq(true)
-  end
 
   it "responds to dock and shows the docked bike" do
     bike = Bike.new
@@ -24,7 +33,6 @@ describe DockingStation do
     bike = Bike.new
     station = DockingStation.new
     station.dock(bike)
-
     expect(subject).to respond_to(:bike)
     expect(station.bike).to eq(bike)
   end
